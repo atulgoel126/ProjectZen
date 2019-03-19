08dba089c280237665e8d4707ce671011234d0aa
Atul Goel <atulg@andrew.cmu.edu>
2019-03-19 00:06:23 -0400

    Procedural Terrain Generation
    Added procedural terrain generation based on vertices and triangles.
    
    Explored normals in Unity.

---
+++ Assets/Scripts/MeshGenerator.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(MeshFilter))]
+public class MeshGenerator : MonoBehaviour {
+
+    public int xSize = 20;
+    public int zSize = 20;
+    public float yMultiplier = 2.0f;
+
+    private Mesh mesh;
+    private Vector3[] vertices;
+    private int[] triangles;
+
+	void Start () {
+        mesh = new Mesh();
+        GetComponent<MeshFilter>().mesh = mesh;
+
+        StartCoroutine(CreateShape());
+	}
+
+    private void Update()
+    {
+        UpdateMesh();
+    }
+
+    private void UpdateMesh()
+    {
+        mesh.Clear();
+        mesh.vertices = vertices;
+        mesh.triangles = triangles;
+        mesh.RecalculateNormals();
+    }
+
+    IEnumerator CreateShape()
+    {
+        vertices = new Vector3[(xSize + 1) * (zSize + 1)];
+
+        int i = 0;
+        for (int z = 0; z <= zSize; z++)
+        {
+            for (int x = 0; x <= xSize; x++)
+            {
+                //Perlin Noise - https://docs.unity3d.com/ScriptReference/Mathf.PerlinNoise.html
+                float y = Mathf.PerlinNoise(x * 0.3f, z * 0.3f) * yMultiplier;
+                vertices[i++] = new Vector3(x, y, z);
+            }
+        }
+        triangles = new int[xSize * zSize * 6];
+
+        int vert = 0;
+        int tris = 0;
+
+        for (int z = 0; z < zSize; z++)
+        {
+            for (int x = 0; x < xSize; x++)
+            {
+                triangles[tris] = vert;
+                triangles[tris + 1] = vert + xSize + 1;
+                triangles[tris + 2] = vert + 1;
+                triangles[tris + 3] = vert + 1;
+                triangles[tris + 4] = vert + xSize + 1;
+                triangles[tris + 5] = vert + xSize + 2;
+
+                vert++;
+                tris = tris + 6;
+
+                yield return new WaitForSeconds(0.01f);
+            }
+            vert++;
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        if (vertices == null)
+        {
+            return;
+        }
+        for (int i = 0; i < vertices.Length; i++)
+        {
+            Gizmos.DrawSphere(vertices[i], 0.1f);
+        }
+    }
+
+}
